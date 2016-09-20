73c04baec529b23fa60751800b154ac6b36ebf7e
Shantanu Madane <madaneshantanu@gmail.com>
2016-09-20 12:12:55 +0530

    Second commit

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express=require('express');
 var app=express();
-var PORT=process.evn.PORT || 5000;
+var PORT=process.env.PORT || 5000;
 
 app.get('/',function(req,res){
 res.send(" TODO API ROOT");
